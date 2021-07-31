188a9140265a356c0d1579cd46cd51fe8dea75f5
unknown <mjlovala@protonmail.com>
2021-07-30 23:09:50 -0600

    test functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-// use serde::Deserialize;
 use std::collections::HashMap;
 
-struct ServiceRequestOpts<'a> {
+pub struct ServiceRequestOpts<'a> {
     token: &'a str,
     method: reqwest::Method,
     body: &'a str,
@@ -11,14 +10,13 @@ struct ServiceRequestOpts<'a> {
 A constant variables for the purpose of calling a designated API.
 Example showed this as a struct, so I went with that.
 */
-struct ServiceConfig<'a> {
-    root_url: &'a str,
+pub struct ServiceConfig<'a> {
+    pub root_url: &'a str,
 }
 
-#[derive(Debug)]
-struct Service<'a> {
+pub struct Service<'a> {
     root_url: &'a str,
-    client: reqwest::Client,
+    pub client: reqwest::Client,
 }
 // An async reqwest Http client that will be used for making all calls
 impl<'a> Service<'a> {
@@ -30,13 +28,15 @@ impl<'a> Service<'a> {
     }
 }
 
-struct ServiceResponse {
-    res: reqwest::Response,
+pub struct ServiceResponse {
+    pub res: reqwest::Response,
 }
 // A standardized way to strip out and return the body of response in a way that can be easily converted to a custom data object.
 impl ServiceResponse {
     #[tokio::main]
-    async fn strip(self) -> Result<HashMap<String, serde_json::Value>, Box<dyn std::error::Error>> {
+    pub async fn strip(
+        self,
+    ) -> Result<HashMap<String, serde_json::Value>, Box<dyn std::error::Error>> {
         let text = self
             .res
             .json::<HashMap<String, serde_json::Value>>()
@@ -45,49 +45,14 @@ impl ServiceResponse {
     }
 }
 
-fn main() {
-    // config incoming from API
-    let config = ServiceConfig {
-        root_url: "https://random-data-api.com/api/beer/random_beer",
-    };
-    let service = setup(config);
-    // request options incoming from API
-    let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(
-        reqwest::header::USER_AGENT,
-        reqwest::header::HeaderValue::from_str("My Rust Program 1.0").unwrap(),
-    );
-    let options = ServiceRequestOpts {
-        token: "mLghicY9MM",
-        method: reqwest::Method::GET,
-        body: "lorem ipsum",
-        headers,
-    };
-
-    let res = match request(&service, &options) {
-        Ok(r) => r,
-        Err(e) => return println!("Error requesting data: {:?}", e),
-    };
-    let response = match res {
-        Some(r) => r,
-        None => panic!(
-            "this REST method is not supported: {:?}",
-            options.method.as_str()
-        ),
-    };
-    let r = ServiceResponse { res: response };
-    let body = r.strip();
-    println!("{:?}", body)
-}
-
 // A setup function that accepts a config object, creates the reqwest::Client instance, and returns the service as a struct.
-fn setup(config: ServiceConfig) -> Service {
+pub fn setup(config: ServiceConfig) -> Service {
     Service::new(&config)
 }
 
 //An async function that will call an api with a provided bearer token and parameters, then return the response as a reqwest::Response object
 #[tokio::main]
-async fn request(
+pub async fn request(
     service: &Service,
     options: &ServiceRequestOpts,
 ) -> Result<Option<reqwest::Response>, Box<dyn std::error::Error>> {
---
+++ tests/tests.rs
@@ -0,0 +1,37 @@
+use ntx_ventures;
+/*
+This function tests the following cases:
+-   Service struct is instantiated succesfully (setup fn)
+-   Client instantiated succesfully (setup fn)
+-   Client works correctly
+    -   Simple http get request status code check
+-   HTTP response can be converted into a custom data object (strip fn)
+*/
+#[tokio::test]
+async fn setup_returns_service() -> Result<(), Box<dyn std::error::Error>> {
+    let config = ntx_ventures::ServiceConfig {
+        root_url: "https://random-data-api.com/api/beer/random_beer",
+    };
+    // test struct can be instantiated
+    // client instantiated succesfully
+    let result = ntx_ventures::setup(config);
+    // test client works correctly
+    let res = result
+        .client
+        .get("https://random-data-api.com/api/beer/random_beer")
+        .send()
+        .await?;
+    // 200 status code
+    assert!(res.status().is_success());
+    // ServiceResponse struct is now instantiated
+    let r = ntx_ventures::ServiceResponse { res };
+    // https://stackoverflow.com/questions/62536566/how-can-i-create-a-tokio-runtime-inside-another-tokio-runtime-without-getting-th
+    tokio::task::spawn_blocking(|| {
+        // unpack response body into a hashmap of <String, T>
+        r.strip();
+    })
+    .await
+    .expect("Task panicked");
+
+    Ok(())
+}
