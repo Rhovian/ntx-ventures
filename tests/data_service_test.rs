6d7ee7604ade54e2e1abfb0e7255dea4f76d3eb9
Mjavala <mjabravo221@gmail.com>
2021-08-08 01:29:22 -0600

    data service test imports

--- tests/data_service_test.rs
+++ tests/data_service_test.rs
@@ -1,4 +1,4 @@
-use ntx_ventures;
+use ntx_ventures::services::data_service;
 /*
 This function tests the following cases:
 -   Service struct is instantiated succesfully (setup fn)
@@ -9,19 +9,19 @@ This function tests the following cases:
 */
 #[tokio::test]
 async fn setup_returns_service() -> Result<(), Box<dyn std::error::Error>> {
-    let config = ntx_ventures::ServiceConfig {
+    let config = data_service::ServiceConfig {
         root_url: "https://random-data-api.com/api/beer/random_beer",
     };
     // test struct can be instantiated
     // client instantiated succesfully
-    let result = ntx_ventures::setup(config);
+    let result = data_service::setup(config);
     // test client works correctly
     let res = result.client.get(result.root_url).send().await?;
     // 200 status code
 
     assert!(res.status().is_success());
     // ServiceResponse struct is now instantiated
-    let r = ntx_ventures::ServiceResponse { res };
+    let r = data_service::ServiceResponse { res };
     // https://stackoverflow.com/questions/62536566/how-can-i-create-a-tokio-runtime-inside-another-tokio-runtime-without-getting-th
     tokio::task::spawn_blocking(|| {
         // unpack response body into a hashmap of <String, T>
