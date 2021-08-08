d5861ac95896942f2116ec744b8640a87cf4426e
unknown <mjlovala@protonmail.com>
2021-08-07 19:04:40 -0600

    refactor data_service

--- src/services/db_service.rs
+++ src/services/db_service.rs
@@ -1,1 +1,12 @@
+/* REQUIREMENTS
+Provide a setup function to establish a connection with credentials.
+Hold a persistent connection to db.
+Provide a query function for sending raw queries to db.
+Provide tests for testing query function for SELECT, ADD, UPDATE, and DELETE functionality.
+Run setup tests to verify that
+connection has been successfully setup
+Insert function is working
+Read function is working
+Delete function is working
+*/
 pub mod db_service {}
