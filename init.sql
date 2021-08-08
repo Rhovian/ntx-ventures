f4b04a8ffb0fa527fbe3b4d5ee977e48f2a98900
Mjavala <mjabravo221@gmail.com>
2021-08-08 01:12:32 -0600

    database init

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS test;
+use test;
+CREATE TABLE example (id INT, name VARCHAR(20));
+INSERT INTO example (id, name) VALUES(1, "ntx-ventures");
+INSERT INTO example (id, name) VALUES(2, "dev");
